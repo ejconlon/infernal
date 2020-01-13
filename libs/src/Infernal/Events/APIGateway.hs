54675a977267eff381403d938a706fbee9f7c793
Eric Conlon <ejconlon@gmail.com>
2020-01-12 21:14:22 -0800

    events and wai

---
+++ libs/src/Infernal/Events/APIGateway.hs
@@ -0,0 +1,71 @@
+module Infernal.Events.APIGateway
+  ( APIGatewayProxyRequest (..)
+  , APIGatewayProxyResponse (..)
+  ) where
+
+import Data.Aeson (object, withObject, (.!=), (.:), (.:?), (.=))
+import Data.Bifunctor (bimap)
+import Data.ByteString (ByteString)
+import qualified Data.CaseInsensitive as CI
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Text as Text
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import Heart.Core.Prelude
+import qualified Network.HTTP.Types as HT
+
+fromAWSQuery :: HashMap Text Text -> HT.Query
+fromAWSQuery = fmap toQueryItem . HashMap.toList where
+  toQueryItem = bimap encodeUtf8 (\x -> if Text.null x then Nothing else Just (encodeUtf8 x))
+
+toAWSHeaders :: HT.ResponseHeaders -> HashMap Text Text
+toAWSHeaders = HashMap.fromList . fmap (bimap (decodeUtf8 . CI.original) decodeUtf8)
+
+fromAWSHeaders :: HashMap Text Text -> HT.RequestHeaders
+fromAWSHeaders = fmap toHeader . HashMap.toList where
+  toHeader = bimap (CI.mk . encodeUtf8) encodeUtf8
+
+data APIGatewayProxyRequest = APIGatewayProxyRequest
+  { _agprqResource              :: !Text
+  , _agprqPath                  :: !ByteString
+  , _agprqHttpMethod            :: !HT.Method
+  , _agprqHeaders               :: !HT.RequestHeaders
+  , _agprqQueryStringParameters :: !HT.Query
+  , _agprqPathParameters        :: !(HashMap Text Text)
+  , _agprqStageVariables        :: !(HashMap Text Text)
+  , _agprqBody                  :: !(Maybe ByteString)
+  } deriving (Eq, Show, Generic)
+
+instance FromJSON APIGatewayProxyRequest where
+  parseJSON =
+    withObject "APIGatewayProxyRequest" $ \o ->
+      APIGatewayProxyRequest
+      <$> o .: "resource"
+      <*> (encodeUtf8 <$> o .: "path")
+      <*> (encodeUtf8 <$> o .: "httpMethod")
+      <*> (fmap fromAWSHeaders <$> o .:? "headers") .!= mempty
+      <*> (fmap fromAWSQuery <$> o .:? "queryStringParameters") .!= mempty
+      <*> o .:? "pathParameters" .!= HashMap.empty
+      <*> o .:? "stageVariables" .!= HashMap.empty
+      <*> (fmap encodeUtf8 <$> o .:? "body")
+
+data APIGatewayProxyResponse = APIGatewayProxyResponse
+  { _agprsStatusCode :: !Int
+  , _agprsHeaders    :: !HT.ResponseHeaders
+  , _agprsBody       :: !(Maybe ByteString)
+  } deriving (Eq, Show)
+
+instance ToJSON APIGatewayProxyResponse where
+  toJSON rep =
+    object
+      [ "statusCode" .= _agprsStatusCode rep
+      , "headers" .= toAWSHeaders (_agprsHeaders rep)
+      , "body" .= fmap decodeUtf8 (_agprsBody rep)
+      ]
+
+instance FromJSON APIGatewayProxyResponse where
+  parseJSON =
+    withObject "APIGatewayProxyResponse" $ \o ->
+      APIGatewayProxyResponse
+        <$> o .: "statusCode"
+        <*> (fromAWSHeaders <$> o .: "headers")
+        <*> (fmap encodeUtf8 <$> o .:? "body")
---
+++ libs/src/Infernal/Wai.hs
@@ -0,0 +1,52 @@
+module Infernal.Wai
+  ( adaptApplication
+  , adaptRequest
+  , adaptResponse
+  ) where
+
+import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
+import Data.Binary.Builder (Builder, toLazyByteString)
+import qualified Data.ByteString as ByteString
+import Data.ByteString.Lazy (toStrict)
+import Data.IORef (modifyIORef, newIORef, readIORef)
+import qualified Data.Text as Text
+import Data.Text.Encoding (decodeUtf8)
+import Heart.Core.Prelude
+import Infernal.Events.APIGateway (APIGatewayProxyRequest (..), APIGatewayProxyResponse (..))
+import Network.Wai (Application, Response, StreamingBody, defaultRequest, responseToStream)
+import Network.Wai.Internal (Request (..), ResponseReceived (..))
+
+adaptRequest :: APIGatewayProxyRequest -> Request
+adaptRequest proxyReq =
+  defaultRequest
+  { requestMethod = _agprqHttpMethod proxyReq
+  , rawPathInfo = _agprqPath proxyReq
+  , pathInfo = Text.split (=='/') (decodeUtf8 (_agprqPath proxyReq))
+  , queryString = _agprqQueryStringParameters proxyReq
+  , requestHeaders = _agprqHeaders proxyReq
+  , requestBody = maybe empty pure (_agprqBody proxyReq)
+  }
+
+consumeStream :: StreamingBody -> IO Builder
+consumeStream sb = do
+  m <- newEmptyMVar
+  r <- newIORef mempty
+  sb (modifyIORef r . flip mappend) (readIORef r >>= putMVar m)
+  takeMVar m
+
+adaptResponse :: MonadIO n => Response -> n APIGatewayProxyResponse
+adaptResponse rep = do
+  let (repStatus, repHeaders, repBodyAction) = responseToStream rep
+  bodyBuilder <- liftIO (repBodyAction consumeStream)
+  let body = toStrict (toLazyByteString bodyBuilder)
+  pure APIGatewayProxyResponse
+    { _agprsStatusCode = fromEnum repStatus
+    , _agprsHeaders = repHeaders
+    , _agprsBody = if ByteString.null body then Nothing else Just body
+    }
+
+adaptApplication :: MonadIO n => Application -> APIGatewayProxyRequest -> n APIGatewayProxyResponse
+adaptApplication app proxyReq = do
+  v <- liftIO newEmptyMVar
+  _ <- liftIO (app (adaptRequest proxyReq) (\res -> adaptResponse res >>= putMVar v >> pure ResponseReceived))
+  liftIO (takeMVar v)
